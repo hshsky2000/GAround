cfe00dc9c7834d77e6e0e9a3aaf27a51b82b866b
Sun Hwang <hshsky2000@gmail.com>
2022-03-09 13:21:32 -0500

    address

--- controllers/index.js
+++ controllers/index.js
@@ -1,18 +1,38 @@
 import { Restaurant } from '../models/restaurant.js'
+import axios from 'axios'
 
 function index(req, res) {
     Restaurant.find({})
     .then(restaurants => {
-      res.render('index', {
-        restaurants,
-        title: "!Restaurants"
-      })
+        getRestaurantsFromYelp()
+            .then(result => {
+                console.log(result.data)
+                res.render('index', {
+                    restaurants: result.data.businesses,
+                    title: "!Restaurants"
+                })
+            })
+            .catch(error => console.log(error))
     })
     .catch(err => {
       console.log(err)
       res.redirect("/restaurants")
     })
-  }
+}
+
+const config = {
+    headers: {
+        Authorization: process.env.YELP_API_KEY
+    }
+}
+
+const url = 'https://api.yelp.com/v3/businesses/search?term=restaurants&location=509 7th st Washington DC&radius=2000'
+
+async function getRestaurantsFromYelp(){
+    return await axios.get(url, config)
+        .then(res => res)
+        .catch(error => console.log(error))
+}
 
 //   async function fetchUser() {
 //     const response = await fetch('https://api.yelp.com/v3/businesses/search?term=restaurants&location=509 7th st Washington DC&radius=2000')
@@ -24,5 +44,4 @@ function index(req, res) {
 
   export{
       index,
-      
   }
--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,3 +1,5 @@
+
+
 function passUserToView(req, res, next) {
     res.locals.user = req.user ? req.user : null
     next()
