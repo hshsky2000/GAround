91ba98ad24ed0ace62fe35b2f9cf64f06337aa9b
Sun Hwang <hshsky2000@gmail.com>
2022-03-10 22:41:03 -0500

    profile

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -14,6 +14,27 @@ function index(req, res) {
     })
   }
 
+  function show(req, res) {
+    Profile.findById(req.params.id)
+    .then((profile) => {
+      Profile.findById(req.user.profile._id)
+      .then(self => {
+        const isSelf = self._id.equals(profile._id)
+        res.render("profiles/show", {
+          title: `profile`,
+          profile,
+          self,
+          isSelf,
+        })
+      })
+    })
+    .catch((err) => {
+      console.log(err)
+      res.redirect("/")
+    })
+  }
+
   export{
-      index
+      index,
+      show
   }
