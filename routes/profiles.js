e51cbe5d38b7e6d71141b63de32ac8137db1cdd8
Sun Hwang <hshsky2000@gmail.com>
2022-03-10 23:01:22 -0500

    arr

--- routes/profiles.js
+++ routes/profiles.js
@@ -3,7 +3,6 @@ import { isLoggedIn } from '../middleware/middleware.js'
 import * as profilesCtrl from '../controllers/profiles.js'
 
 const router = Router()
-router.get('/', isLoggedIn, )
 router.get('/', isLoggedIn, profilesCtrl.index)
 router.get('/:id', isLoggedIn, profilesCtrl.show)
 
